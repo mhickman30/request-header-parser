8bcb9f0450d270f656420c4b2659495656136bf1
Matt Hickman <matthickman10@gmail.com>
2017-04-04 23:59:10 +0000

    Done

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+//Used dting on Github to help with OS match function
+
 var express = require("express");
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hey World!');
+  var IPaddress = req.headers["x-forwarded-for"];
+  var lang = req.headers["accept-language"].split(",")[0];
+  var OS = req.headers["user-agent"].match(/\((.*?)\)/)[1];
+  
+  res.json({
+      ipaddress: IPaddress,
+      language: lang,
+      software: OS
+  });
 })
 
 app.listen(8080, function () {
